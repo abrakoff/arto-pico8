91ceb49b67c2b73afc7f7281ddce9b1a5267d367
TC Fraser <tcfraser237@gmail.com>
2025-07-30 21:39:40 +0200

    changed the behaviour of the simulation speed changer and made it display the actual simulation speed

--- controls.lua
+++ controls.lua
@@ -1,57 +1,67 @@
 -- controls
 function init_controls()
     update_logic_clock = 0
-    sim_speeds = {32,16,8,4,2,1,1/2,1/4,1/8,1/16,1/32}
+    sim_speeds        = {32,16,8,4,2,1,1/2,1/4,1/8,1/16,1/32}
+    sim_speed_strings = {"/32","/16","/8","/4","/2","X1","X2","X4","X8","X16","X32"}
     is_paused  = true
     step_counter  = 0
     sim_speed_idx = 1 
 
     controls = {}
-    local x_offset = 1
-    add_control_button(pos(x_offset,68), default_control_draw(16), step)
-    x_offset += 10
-    add_control_button(pos(x_offset,68), draw_play_pause, play_pause)
-    x_offset += 10
-    add_control_button(pos(x_offset,68), default_control_draw(18), increase_speed, decrease_speed)
-    x_offset += 10
-    add_control_button(pos(x_offset,68), default_control_draw(21), reset_level)
-    x_offset += 57 
+    local offset = pos(1,68)
+    add_control_button(default_control_box(offset), default_control_draw(16), step)
+    offset.x += 10
+    add_control_button(default_control_box(offset), draw_play_pause, play_pause)
+    offset.x += 10
+    add_control_button(box(offset.x, offset.y, offset.x+12, offset.y+7), draw_sim_speed, increase_speed, decrease_speed)
+    offset.x += 15
+    add_control_button(default_control_box(offset), default_control_draw(21), reset_level)
+    offset.x += 51 
 
-    add_control_button(pos(x_offset,68), default_control_draw(20), previous_level)
-    x_offset += 10
-    add_control_button(pos(x_offset,68), draw_level_number)
-    x_offset += 11
-    add_control_button(pos(x_offset,68), default_control_draw(22), next_level)
-    x_offset += 10
-    add_control_button(pos(x_offset,68), default_control_draw(23), randomize_all_callback)
+    add_control_button(default_control_box(offset), default_control_draw(20), previous_level)
+    offset.x += 10
+    add_control_button(box(offset.x, offset.y, offset.x+8, offset.y+7), draw_level_number)
+    offset.x += 11
+    add_control_button(default_control_box(offset), default_control_draw(22), next_level)
+    offset.x += 10
+    add_control_button(default_control_box(offset), default_control_draw(23), randomize_all_callback)
 end
 
-function add_control_button(p, draw_func, left_func, right_func)
-    local button_box = box(p.x, p.y, p.x+7, p.y+7)
+function add_control_button(button_box, draw_func, left_func, right_func)
     add_clickable(button_box, left_func, right_func, "levels")
-    add(controls, {p=p, draw=draw_func})
+    add(controls, {box=button_box, draw=draw_func})
+end
+
+function default_control_box(p)
+    return box(p.x, p.y, p.x+7, p.y+7)
 end
 
 function default_control_draw(sprite_idx)
-    return function (control) spr(sprite_idx, control.p.x, control.p.y) end
+    return function (control) spr(sprite_idx, control.box.l, control.box.t) end
 end
 
 function draw_play_pause(control)
     if is_paused then
-        spr(17, control.p.x, control.p.y)
+        spr(17, control.box.l, control.box.t)
     else
-        spr(19, control.p.x, control.p.y)
+        spr(19, control.box.l, control.box.t)
     end
 end
 
 function draw_level_number(control)
-    rectfill(control.p.x, control.p.y, control.p.x+8, control.p.y+7,6)
-    rectfill(control.p.x, control.p.y, control.p.x+8, control.p.y+6,7)
+    rectfill(control.box.l, control.box.t, control.box.r, control.box.b,6)
+    rectfill(control.box.l, control.box.t, control.box.r, control.box.b-1,7)
     if level_idx >= 10 then
-        print(level_idx, control.p.x+1, control.p.y+1, 0)
+        print(level_idx, control.box.l+1, control.box.t+1, 0)
     else
-        print(level_idx, control.p.x+3, control.p.y+1, 0)
+        print(level_idx, control.box.l+3, control.box.t+1, 0)
     end
+end
+
+function draw_sim_speed(control)
+    rectfill(control.box.l, control.box.t, control.box.r, control.box.b,6)
+    rectfill(control.box.l, control.box.t, control.box.r, control.box.b-1,7)
+    print(sim_speed_strings[sim_speed_idx], control.box.l+1, control.box.t+1, 0)
 end
 
 function draw_controls()
@@ -94,13 +104,11 @@ function step()
 end
 
 function increase_speed()
-    sim_speed_idx = ((sim_speed_idx-1+1) % #sim_speeds) + 1
-    play()
+    sim_speed_idx = min(sim_speed_idx+1, #sim_speeds)
 end
 
 function decrease_speed()
-    sim_speed_idx = ((sim_speed_idx-1-1) % #sim_speeds) + 1
-    play()
+    sim_speed_idx = max(sim_speed_idx-1, 1)
 end
 
 function next_level()
